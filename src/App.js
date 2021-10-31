9eddb5478fc846bc1b99713a657f8efaac30157c
Vishal Modanwal <modanwalvishal@gmail.com>
2021-10-31 15:42:55 +0530

    ADDED: Routing to pages done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Instruments from "./Instruments";
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Quotes from "./Quotes";
 function App() {
   return (
     <div className="App">
-      <Instruments />
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Instruments} />
+          <Route path="/quotes" component={Quotes} />
+        </Switch>
+      </Router>
     </div>
   );
 }
